893c8d22eaf242cbf4c975093c19be8af11af03d
Fberekettussa123 <berekettussa116>
2022-07-09 17:55:13 +0300

    asd

--- src/views/components/stage1/Hero.js
+++ src/views/components/stage1/Hero.js
@@ -20,29 +20,35 @@ function Hero(props) {
   });
 
   return (
-    <div className="hero-container">
-      <div className={`hero-container-inner${scroll ? ' scrolled' : ''}`}>
-        <header className="page-header">
-          <div className="half">
-            <h2>Title</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
-              amet nisl nec massa egestas ornare. Nulla volutpat, ante ac
-              sodales luctus, nisi dui varius purus, eleifend lacinia justo
-              tellus a sem. Nunc at porta odio.
-            </p>
-          </div>
-          <div className='button-container' id="Navigation">
-            <div className="cta-container cta-hero">
-              <button className="cta " onClick={props.handleOpen}>
-                <div><p className="">Call To Action</p> </div>
-                <div><span>&rarr;</span></div>
-                </button>
+    <div>
+      <div className="hero-container">
+        <div className={`hero-container-inner${scroll ? ' scrolled' : ''}`}>
+          <header className="page-header">
+            <div className="half">
+              <h2>Title</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
+                amet nisl nec massa egestas ornare. Nulla volutpat, ante ac
+                sodales luctus, nisi dui varius purus, eleifend lacinia justo
+                tellus a sem. Nunc at porta odio.
+              </p>
             </div>
-          </div>
-        </header>
-      {/* <div className="gradient"></div> */}
-      </div>
+            <div className="button-container" id="Navigation">
+              <div className="cta-container cta-hero">
+                <button className="cta " onClick={props.handleOpen}>
+                  <div>
+                    <p className="">Call To Action</p>{' '}
+                  </div>
+                  <div>
+                    <span>&rarr;</span>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </header>
+          {/* <div className="gradient"></div> */}
+        </div>
+        </div>
         <Filters filters={['Filter 1', 'Filter 2', 'Filter 3', 'Filter 4']} />
     </div>
   );
