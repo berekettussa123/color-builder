3fd964925f9291bed59805392041b755ecd897b3
Fberekettussa123 <berekettussa116>
2022-07-10 14:27:08 +0300

    asd

--- src/views/First/StageFirst.js
+++ src/views/First/StageFirst.js
@@ -31,7 +31,7 @@ function StageFirst() {
     <div>
       <Nav />
       <MainContainer>
-        <div style={{width:'100%',display:'flex',flexDirection:'column',alignItems:'flex-start'}}>
+        <div style={{width:'100%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
           <Hero handleOpen={showBuildModal} />
           <section className="elements">
             <div className="elements-container">
